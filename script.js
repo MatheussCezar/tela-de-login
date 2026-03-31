493e58fadbcbfd423e292c6d99c978640812ec19
Matheus Cezar <matheuscezar58@gmail.com>
2026-03-31 16:41:30 -0300

    formatação de campo

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const cidade = document.getElementById("cidade");
 const estado = document.getElementById("estado")
 const complemento = document.getElementById("complemento")
 
-async function buscaDadosCEP(CEP){
+async function buscaDadosCEP(CEP){ 
     try {
         const response = await fetch(`https://viacep.com.br/ws/${CEP}/json/`);
         data = await response.json();
@@ -37,3 +37,50 @@ campoCEP.addEventListener("input", function(){
         buscaDadosCEP(CEP);
     }
 })
+
+const estadoCivil = document.getElementById("estadoCivil");
+const camposConjuge = document.getElementById("conjuge");
+
+estadoCivil.addEventListener("change", function(){
+    if(estadoCivil.value == "1" || estadoCivil.value == "3"){
+        camposConjuge.style = '';
+    } else {
+        camposConjuge.style = "display: none;"
+    }
+})
+
+
+
+document.querySelectorAll(".CPF").forEach(element => {
+    element.addEventListener('input', function(e) { //Deixa o espaçamento do CPF ". e -"
+        let v = e.target.value.replace(/\D/g, '');
+        v = v.replace(/(\d{3})(\d)/, '$1.$2');
+        v = v.replace(/(\d{3})(\d)/, '$1.$2');
+        v = v.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+        e.target.value = v;
+    })
+});
+
+document.querySelectorAll(".RG").forEach(element => { 
+    element.addEventListener('input', function(e) { //Deixa o espaçamento do RG ". e-"
+        let v = e.target.value.replace(/\D/g, '');
+        v = v.replace(/(\d{2})(\d)/, '$1.$2');
+        v = v.replace(/(\d{3})(\d)/, '$1.$2');
+        v = v.replace(/(\d{3})(\d{1})$/, '$1-$2');
+        e.target.value = v;
+    });
+});
+
+document.getElementById("telefone").addEventListener('input', function(e) { // Deixa o númerocertinho "+55 (99) 9 9999-9999"
+    let v = e.target.value.replace(/\D/g, '');
+    v = v.replace(/^(\d{2})(\d)/, '+$1 ($2');
+    v = v.replace(/^(\+\d{2} \(\d{2})(\d)/, '$1) $2');
+    v = v.replace(/(\d{1})(\d{4})(\d{4})$/, '$1 $2-$3');
+    e.target.value = v;
+});
+
+document.getElementById("CEP").addEventListener('input', function(e) { // Deixa o CEP com "-"
+    let v = e.target.value.replace(/\D/g, ''); 
+    v = v.replace(/(\d{5})(\d)/, '$1-$2'); 
+    e.target.value = v;
+});
