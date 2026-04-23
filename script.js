f99bd5d7388c8b2f8e016313bc09e3fcc190240f
Matheus Cezar <matheuscezar58@gmail.com>
2026-04-22 23:34:09 -0300

    Correção de bug

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ document.querySelectorAll(".RG").forEach(element => {
         let v = e.target.value.replace(/\D/g, '');
         v = v.replace(/(\d{2})(\d)/, '$1.$2');
         v = v.replace(/(\d{3})(\d)/, '$1.$2');
-        v = v.replace(/(\d{3})(\d{1})$/, '$1-$2');
+        v = v.replace(/(\d{3})(\d{2})$/, '$1-$2');
         e.target.value = v;
     });
 });
